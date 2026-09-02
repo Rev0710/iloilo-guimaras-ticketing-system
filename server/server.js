238ef633e68ef17450c08c44094306480ce15ae3
ztan-sh <cariasnathan24@gmail.com>
2026-09-02 18:12:41 +0800

    Allow Vercel frontend in CORS

--- server/server.js
+++ server/server.js
@@ -20,7 +20,8 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "http://localhost:5174"
+            "http://localhost:5174",
+            "https://mobile-base-ticketing-system-5sy2.vercel.app"
         ],
         credentials: true
     })
